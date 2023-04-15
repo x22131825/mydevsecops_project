2cf66c61aa210bf88d08b3e4f8989e7a1784f89d
x22131825 <124701405+x22131825@users.noreply.github.com>
2023-04-15 18:30:55 +0100

    Update nginx.sh

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,9 +4,9 @@
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
-sudo cp /home/ubuntu/mydevsecproject/nginx/nginx.conf /etc/nginx/sites-available/blog
-sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled/
-#sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
+sudo cp /home/ubuntu/mydevsecproject/mydevsecproject/nginx/nginx.conf /etc/nginx/sites-available/blog
+sudo ln -s /etc/nginx/sites-available/mydevsecproject/mydevsecproject /etc/nginx/sites-enabled/
+#sudo ln -s /etc/nginx/sites-available/mydevsecproject/mydevsecproject /etc/nginx/sites-enabled
 #sudo nginx -t
 sudo gpasswd -a www-data ubuntu
 sudo systemctl restart nginx
